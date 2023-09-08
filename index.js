317cbb62f61de181e7335e7a3e97f7645a81139b
Artur Oganesian <artur.hovanesian@gmail.com>
2023-09-08 09:59:07 +0400

    first

---
+++ index.js
@@ -0,0 +1,129 @@
+let button1 = document.getElementById("green1");
+let button2 = document.getElementById("green2");
+let button3 = document.getElementById("green3");
+let button4 = document.getElementById("green4");
+let button5 = document.getElementById("blue1");
+let button6 = document.getElementById("blue2");
+let button7 = document.getElementById("blue3");
+let button8 = document.getElementById("blue4");
+let button9 = document.getElementById("blue5");
+let button10 = document.getElementById("blue6");
+const textButton1 = document.getElementById("text1")
+const textButton2 = document.getElementById("text2")
+const textButton3 = document.getElementById("text3")
+const textButton4 = document.getElementById("text4")
+const textButton5 = document.getElementById("text5")
+const textButton6 = document.getElementById("text6")
+const textButton7 = document.getElementById("text7")
+const textButton8 = document.getElementById("text8")
+const textButton9 = document.getElementById("text9")
+const textButton10 = document.getElementById("text10")
+const textShow1 = () => {
+    textButton1.style.display = 'block'
+    button1.style.display = 'none'
+};
+const buttonShow1 = () => {
+    textButton1.style.display = 'none'
+    button1.style.display = 'block'
+}
+button1.addEventListener("click", textShow1)
+textButton1.addEventListener("click", buttonShow1)
+    //______________________________________________________
+const textShow2 = () => {
+    textButton2.style.display = 'block'
+    button2.style.display = 'none'
+};
+const buttonShow2 = () => {
+    textButton2.style.display = 'none'
+    button2.style.display = 'block'
+}
+button2.addEventListener("click", textShow2)
+textButton2.addEventListener("click", buttonShow2)
+    //_________________________________________________________
+const textShow3 = () => {
+    textButton3.style.display = 'block'
+    button3.style.display = 'none'
+};
+const buttonShow3 = () => {
+    textButton3.style.display = 'none'
+    button3.style.display = 'block'
+}
+button3.addEventListener("click", textShow3)
+textButton3.addEventListener("click", buttonShow3)
+    //_________________________________________________________
+const textShow4 = () => {
+    textButton4.style.display = 'block'
+    button4.style.display = 'none'
+};
+const buttonShow4 = () => {
+    textButton4.style.display = 'none'
+    button4.style.display = 'block'
+}
+button4.addEventListener("click", textShow4)
+textButton4.addEventListener("click", buttonShow4)
+    //____________________________________________
+const textShow5 = () => {
+    textButton5.style.display = 'block'
+    button5.style.display = 'none'
+};
+const buttonShow5 = () => {
+    textButton5.style.display = 'none'
+    button5.style.display = 'block'
+}
+button5.addEventListener("click", textShow5)
+textButton5.addEventListener("click", buttonShow5)
+    //___________________________________________________
+const textShow6 = () => {
+    textButton6.style.display = 'block'
+    button6.style.display = 'none'
+};
+const buttonShow6 = () => {
+    textButton6.style.display = 'none'
+    button6.style.display = 'block'
+}
+button6.addEventListener("click", textShow6)
+textButton6.addEventListener("click", buttonShow6)
+    //_______________________________________________________
+const textShow7 = () => {
+    textButton7.style.display = 'block'
+    button7.style.display = 'none'
+};
+const buttonShow7 = () => {
+    textButton7.style.display = 'none'
+    button7.style.display = 'block'
+}
+button7.addEventListener("click", textShow7)
+textButton7.addEventListener("click", buttonShow7)
+    //__________________________________________________
+const textShow8 = () => {
+    textButton8.style.display = 'block'
+    button8.style.display = 'none'
+};
+const buttonShow8 = () => {
+    textButton8.style.display = 'none'
+    button8.style.display = 'block'
+}
+button8.addEventListener("click", textShow8)
+textButton8.addEventListener("click", buttonShow8)
+    //____________________________________________________-
+const textShow9 = () => {
+    textButton9.style.display = 'block'
+    button9.style.display = 'none'
+};
+const buttonShow9 = () => {
+    textButton9.style.display = 'none'
+    button9.style.display = 'block'
+}
+button9.addEventListener("click", textShow9)
+textButton9.addEventListener("click", buttonShow9)
+    //____________________________________________________
+const textShow10 = () => {
+    textButton10.style.display = 'block'
+    button10.style.display = 'none'
+};
+const buttonShow10 = () => {
+    textButton10.style.display = 'none'
+    button10.style.display = 'block'
+}
+button10.addEventListener("click", textShow10)
+textButton10.addEventListener("click", buttonShow10)
